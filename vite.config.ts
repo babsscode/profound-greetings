ef96fbf14173a12c838b7fa957cb4baccd4dc224
babs14 <babitha14karre@gmail.com>
2024-12-24 18:46:07 -0500

    cloudflare

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    target: 'esnext', // Ensure modern JavaScript features are supported
+    outDir: 'dist',   // Ensure Vite outputs to the correct directory
+  },
   plugins: [react()],
 })
