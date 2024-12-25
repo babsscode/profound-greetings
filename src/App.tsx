caa768fddfe833330b1e0a2265f5cd6298ae5d42
babs14 <babitha14karre@gmail.com>
2024-12-24 23:27:46 -0500

    analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import MainRoutes from './Routes'
 import './index.css'; 
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
     <div>
       <MainRoutes/>
+      <Analytics />
     </div>
   )
 }
